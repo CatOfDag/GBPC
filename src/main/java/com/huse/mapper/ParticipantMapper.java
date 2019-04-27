f14aa46bddb0ff741f95ccfbb12290d978577ad2
clin <814581942@qq.com>
2019-04-27 11:01:13 +0800

    功能完善

---
+++ src/main/java/com/huse/mapper/ParticipantMapper.java
@@ -0,0 +1,23 @@
+package com.huse.mapper;
+
+import com.huse.pojo.Participant;
+
+import java.util.List;
+
+public interface ParticipantMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Participant record);
+
+    int insertSelective(Participant record);
+
+    Participant selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Participant record);
+
+    int updateByPrimaryKey(Participant record);
+
+    List<Participant> getParticipantLists(int start, int limit);
+
+    int count();
+}
